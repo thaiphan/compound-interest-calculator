ffaa4b506bb20aae71fc5572488ad9303afc85dc
Thai Phan <thai@outlook.com>
2019-03-16 15:30:55 +1100

    Move results chart into separate component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 import './App.scss'
-import getCompoundInterest from './helpers/compound-interest'
-import getIncomeTax from './helpers/income-tax'
-import BarChart from './components/BarChart'
 import MoneyInput from './components/MoneyInput'
 import PercentInput from './components/PercentInput'
 import YearInput from './components/YearInput'
+import Results from './components/Results'
 
 class App extends Component {
   constructor (props) {
@@ -21,117 +19,7 @@ class App extends Component {
     }
   }
 
-  handleFormChange = event => {
-    let newValue = event.target.value
-    switch (event.target.name) {
-      case 'numberOfYears':
-        if (newValue < 0) {
-          newValue = 0
-        } else if (newValue > 60) {
-          newValue = 60
-        }
-        break
-      default:
-        newValue = event.target.value
-    }
-
-    this.setState({
-      [event.target.name]: newValue
-    })
-  }
-
-  updateInitialDeposit = initialDeposit => {
-    this.setState({
-      initialDeposit: initialDeposit
-    })
-  }
-
-  updateRegularDeposit = regularDeposit => {
-    this.setState({
-      regularDeposit: regularDeposit
-    })
-  }
-
-  updateAnnualIncome = annualIncome => {
-    this.setState({
-      annualIncome: annualIncome
-    })
-  }
-
-  updateInterestRate = interestRate => {
-    this.setState({
-      interestRate: interestRate
-    })
-  }
-
-  updateNumberOfYears = numberOfYears => {
-    this.setState({
-      numberOfYears: numberOfYears
-    })
-  }
-
-  handleToggle = event => {
-    this.setState({
-      [event.target.name]: !this.state[event.target.name]
-    })
-  }
-
-  getYearlyInterest () {
-    let yearlyInterest = []
-
-    let currentPrincipal = this.state.initialDeposit
-    let totalInterest = 0
-    let regularDeposits = 0
-    for (let i = 1; i <= this.state.numberOfYears; i++) {
-      regularDeposits += this.state.regularDeposit * 12
-
-      let grossPrincipal = getCompoundInterest(currentPrincipal, this.state.regularDeposit, this.state.interestRate, 12)
-
-      // The amount of money made this year in interest
-      let capitalGains = grossPrincipal - currentPrincipal - (this.state.regularDeposit * 12)
-      totalInterest += capitalGains
-
-      // Your money after you've accrued your interest for the year
-      currentPrincipal = grossPrincipal
-
-      // How much tax the government deserves from your earnt interest
-      let capitalGainsTax = 0
-      if (this.state.applyTax) {
-        capitalGainsTax = this.calculateCapitalGainsTax(capitalGains, +this.state.annualIncome)
-      }
-
-      // Your money after the government receives its entitled share of your money
-      currentPrincipal -= capitalGainsTax
-
-      totalInterest -= capitalGainsTax
-
-      yearlyInterest = yearlyInterest.concat({
-        name: `Year ${i}`,
-        principal: Math.round(currentPrincipal),
-        regularDeposits: regularDeposits,
-        capitalGainsTax: Math.round(capitalGainsTax),
-        totalInterest: Math.round(totalInterest)
-      })
-    }
-
-    return yearlyInterest
-  }
-
-  calculateCapitalGainsTax = (capitalGains, annualIncome) => {
-    let grossIncomeTax = getIncomeTax(annualIncome + capitalGains)
-    let incomeTax = getIncomeTax(annualIncome)
-
-    return grossIncomeTax - incomeTax
-  }
-
   render () {
-    let data = {
-      initialDeposit: this.getYearlyInterest().map(yearlyInterest => this.state.initialDeposit),
-      regularDeposits: this.getYearlyInterest().map(yearlyInterest => yearlyInterest.regularDeposits),
-      totalInterest: this.getYearlyInterest().map(yearlyInterest => yearlyInterest.totalInterest),
-      capitalGainsTax: this.getYearlyInterest().map(yearlyInterest => yearlyInterest.capitalGainsTax)
-    }
-
     return (
       <div className="App">
         <div className="pure-g">
@@ -188,16 +76,73 @@ class App extends Component {
               />
             )}
           </div>
-          <div className="content pure-u-1 pure-u-md-4-5">
-            <h2>Results</h2>
-            <BarChart
-              labels={this.getYearlyInterest().map(yearlyInterest => yearlyInterest.name)}
-              data={data}
-            />
-          </div>
+
+          <Results
+            initialDeposit={this.state.initialDeposit}
+            regularDeposit={this.state.regularDeposit}
+            numberOfYears={this.state.numberOfYears}
+            interestRate={this.state.interestRate}
+            applyTax={this.state.applyTax}
+            annualIncome={this.state.annualIncome}
+          />
         </div>
       </div>
     )
+  }
+
+  handleFormChange = event => {
+    let newValue = event.target.value
+    switch (event.target.name) {
+      case 'numberOfYears':
+        if (newValue < 0) {
+          newValue = 0
+        } else if (newValue > 60) {
+          newValue = 60
+        }
+        break
+      default:
+        newValue = event.target.value
+    }
+
+    this.setState({
+      [event.target.name]: newValue
+    })
+  }
+
+  updateInitialDeposit = initialDeposit => {
+    this.setState({
+      initialDeposit: initialDeposit
+    })
+  }
+
+  updateRegularDeposit = regularDeposit => {
+    this.setState({
+      regularDeposit: regularDeposit
+    })
+  }
+
+  updateAnnualIncome = annualIncome => {
+    this.setState({
+      annualIncome: annualIncome
+    })
+  }
+
+  updateInterestRate = interestRate => {
+    this.setState({
+      interestRate: interestRate
+    })
+  }
+
+  updateNumberOfYears = numberOfYears => {
+    this.setState({
+      numberOfYears: numberOfYears
+    })
+  }
+
+  handleToggle = event => {
+    this.setState({
+      [event.target.name]: !this.state[event.target.name]
+    })
   }
 }
 
